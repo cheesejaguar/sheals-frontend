b629d4c9c2c3263fe2e3d9a3df1e9515164dd0b7
Aaron <4625152+cheesejaguar@users.noreply.github.com>
2023-03-20 12:37:08 -0700

    remove trailing slash from API endpoint

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const contentElement = document.getElementById('content');
 
 const fetchData = async () => {
     try {
-        const response = await fetch('https://api.shealsgames.com/top3/', {method: 'get'});
+        const response = await fetch('https://api.shealsgames.com/top3', {method: 'get'});
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
